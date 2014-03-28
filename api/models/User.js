5bbf894e282b99a26c11ccdcb6b08276542f7f96
Daniel Baak <daniel.baak@gmail.com>
2014-03-28 17:52:14 +0100

    added blank queue mgmt

---
+++ api/models/User.js
@@ -0,0 +1,23 @@
+/**
+ * User.js
+ *
+ * @description :: TODO: You might write a short summary of how this model works and what it represents here.
+ * @docs		:: http://sailsjs.org/#!documentation/models
+ */
+
+module.exports = {
+
+	attributes: {
+    username: {
+      type: 'STRING',
+      required: true,
+      maxLength: 20,
+    },
+    password: {
+      type: 'STRING',
+      minLength: 6,
+      required: true,
+    }
+	}
+
+};
