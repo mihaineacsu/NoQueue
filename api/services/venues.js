18cf709aaabd27017b35bf089a0d084387040589
Daniel Baak <daniel.baak@gmail.com>
2014-03-29 07:07:26 +0100

    removed unfinished venue integration

--- api/services/venues.js
+++ api/services/venues.js
@@ -1,26 +1,28 @@
-var https = require('https');
-
-.request(options, function(response) {
-    var responseData = '';
-    response.setEncoding('utf8');
-
-    response.on('data', function(chunk){
-      responseData += chunk;
-    });
-
-    response.once('error', function(err){
-      // Some error handling here, e.g.:
-      res.serverError(err);
-    });
-
-    response.on('end', function(){
-      try {
-        // response available as `responseData` in `yourview`
-        res.locals.requestData = JSON.parse(responseData);
-      } catch (e) {
-        sails.log.warn('Could not parse response from options.hostname: ' + e);
-      }
-
-      res.view('yourview');
-    });
-  }).end();
+//var https = require('https');
+//
+//var options = {};
+//
+//https.request(options, function(response) {
+//    var responseData = '';
+//    response.setEncoding('utf8');
+//
+//    response.on('data', function(chunk){
+//      responseData += chunk;
+//    });
+//
+//    response.once('error', function(err){
+//      // Some error handling here, e.g.:
+//      res.serverError(err);
+//    });
+//
+//    response.on('end', function(){
+//      try {
+//        // response available as `responseData` in `yourview`
+//        res.locals.requestData = JSON.parse(responseData);
+//      } catch (e) {
+//        sails.log.warn('Could not parse response from options.hostname: ' + e);
+//      }
+//
+//      res.view('yourview');
+//    });
+//  }).end();
